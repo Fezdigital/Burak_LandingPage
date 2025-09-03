c86ab522a2d3ec80c3325c4169b3096865553403
Burakcan <burakcanarisoy@gmail.com>
2025-09-03 22:41:00 +0300

    Revert "unnecessary project categories removed"
    This reverts commit d3118e7efd4f80401392441a9eadc424bccc28b7.

--- src/types/project.ts
+++ src/types/project.ts
@@ -17,6 +17,8 @@ export enum ProjectCategory {
   WEB_DEVELOPMENT = 'WEB_DEVELOPMENT',
   ANDROID_DEVELOPMENT = 'ANDROID_DEVELOPMENT',
   DATABASE = 'DATABASE',
+  FULL_STACK = 'FULL_STACK',
+  OTHER = 'OTHER',
 }
 
 export interface CreateProjectData {
